b9d0a3ca7804fce15efbc9cb6254a671f4a22e8e
Yoelrobin <ro.villa@duocuc.cl>
2022-04-28 10:15:48 -0400

    confirmar

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,4 +1,21 @@
 function vNombre(){
-    var cantidad = document.getElementById('cadena').value.length;
-    if()
+    
+    var cantidad = document.getElementById('nombre').value.length
+
+    if(cantidad <3){
+        document.getElementById('mensaje').innerHTML = 'Pocos Caracteres'
+        document.getElementById('mensaje').style.color = 'red'
+        document.getElementById('enviar').style.display = 'none'
+    }
+    if(cantidad >=3 && cantidad <=16){
+        document.getElementById('mensaje').innerHTML = 'Correcto'
+        document.getElementById('mensaje').style.color = 'green'
+        document.getElementById('enviar').style.display = 'block'
+    }
+    if(cantidad >16){
+        document.getElementById('mensaje').innerHTML = 'Muchos Caracteres'
+        document.getElementById('mensaje').style.color = 'red'
+        document.getElementById('enviar').style.display = 'none'
+    }
 }
+
